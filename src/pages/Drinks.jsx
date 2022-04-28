2d470c5b01aef0ba15e4c9c528a493ec4eaefa63
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-04-28 14:25:59 -0300

    25 e 26 ok

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
+import AppFoodContext from '../context/AppFoodContext';
+// import linkApi from '../services/linkApi';
 
 function Drinks() {
+  const { recipeDrinks, drinkCategories } = useContext(AppFoodContext);
+
+  if (recipeDrinks) console.log(recipeDrinks);
+
   return (
     <>
       <Header title="Drinks" btnSearch />
       <h1>Drinks</h1>
+      {recipeDrinks?.slice(0,12)
+      .map((drinks, index) => (
+        <div key={ drinks.idDrink }>
+          <p data-testid={`${index}-card-name`}>{drinks.strDrink}</p>
+          <img data-testid={`${index}-card-img`} src={ drinks.strDrinkThumb } alt={ drinks.strDrink } />
+          <p data-testid={`${index}-recipe-card`}>{drinks.strInstructions}</p>
+        </div>
+      ))}
+      {drinkCategories?.slice(0,5)
+      .map((cat) => (
+        <button
+          key={ cat.strCategory }
+          type="submit"
+          data-testid={ `${cat.strCategory}-category-filter` }
+        >
+          {cat.strCategory}
+        </button>
+      ))}
     </>
   );
 }
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
+import AppFoodContext from '../context/AppFoodContext';
 
 function Foods() {
+  const {
+    foodCategories,
+    recipeFoods,
+    recipeDrinks,
+  } = useContext(AppFoodContext);
+
+  if(recipeFoods) console.log(recipeFoods);
+
   return (
     <>
       <Header title="Foods" btnSearch />
-      <h1>Foods</h1>
+      {recipeFoods?.slice(0,12)
+      .map((meal, index) => (
+        <div key={ meal.idMeal }>
+          <p data-testid={`${index}-card-name`}>{meal.strMeal}</p>
+          <img data-testid={`${index}-card-img`} src={ meal.strMealThumb } alt={ meal.strMeal } />
+          <p data-testid={`${index}-recipe-card`}>{meal.strInstructions}</p>
+        </div>
+      ))}
+      {foodCategories?.slice(0,5)
+      .map((cat) => (
+        <button
+          key={ cat.strCategory }
+          type="submit"
+          data-testid={ `${cat.strCategory}-category-filter` }
+        >
+          {cat.strCategory}
+        </button>
+      ))}
     </>
   );
 }
