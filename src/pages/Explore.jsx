0317ded21f8abc6795ec8352b942a259c0b00a86
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-05-02 17:53:54 -0300

    criando tela de explore - exercicios 67 ,68 e 69 ok

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-function Explore() {
+function Explore({ history }) {
   return (
     <>
       <Header title="Explore Foods" />
-      <h1>Explore</h1>
+      <button
+        data-testid="explore-foods"
+        type="submit"
+        onClick={ () => history.push('/explore/foods') }
+      >
+        Explore Foods
+      </button>
+      <button
+        data-testid="explore-drinks"
+        type="submit"
+        onClick={ () => history.push('/explore/drinks') }
+      >
+        Explore Drinks
+      </button>
       <Footer />
     </>
   );
 }
+
+Explore.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 
 export default Explore;
