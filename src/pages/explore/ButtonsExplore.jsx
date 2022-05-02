88f6442fb5fd7c29ccac231661ece183b0eaf284
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-05-02 17:34:02 -0300

    button naionality e surprise

--- src/pages/explore/ButtonsExplore.jsx
+++ src/pages/explore/ButtonsExplore.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import apiRequestByLink from '../../services/apiRequestByLink';
 
-function ButtonsExplore({ history, food }) {
+function ButtonsExplore({ food }) {
+  const history = useHistory();
+
   function clickExploreIngredient() {
     if (food) {
       history.push('/explore/foods/ingredients');
     } else history.push('/explore/drinks/ingredients');
   }
 
-  console.log(food);
+  function clickExploreNationality() {
+    if (food) {
+      history.push('/explore/foods/nationalities');
+    } else history.push('/explore/drinks/nationalities');
+  }
+
+  async function clickSurprise() {
+    let idAleatoria = '';
+    if (food) {
+      idAleatoria = await apiRequestByLink('https://www.themealdb.com/api/json/v1/1/random.php');
+      history.push(`/foods/${idAleatoria.meals[0].idMeal}`);
+    } else {
+      idAleatoria = await apiRequestByLink('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+      console.log(idAleatoria);
+      history.push(`/drinks/${idAleatoria.drinks[0].idDrink}`);
+    }
+  }
+
   return (
     <>
       <button
@@ -23,6 +44,7 @@ function ButtonsExplore({ history, food }) {
           <button
             type="submit"
             data-testid="explore-by-nationality"
+            onClick={ clickExploreNationality }
           >
             By Nationality
           </button>)
@@ -30,6 +52,7 @@ function ButtonsExplore({ history, food }) {
       <button
         type="submit"
         data-testid="explore-surprise"
+        onClick={ clickSurprise }
       >
         Surprise me!
       </button>
