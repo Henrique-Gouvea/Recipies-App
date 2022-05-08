b5a44b52fda5e5d1fef264b39206b2dc6c583215
Robério Almeida <dev.roberio@gmail.com>
2022-05-07 23:28:33 -0300

    feat: testa se email e password válidos são digitados

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { screen, cleanup, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithContext from '../renderWithContext';
 import App from '../App';
+
+const EMAIL_ID = 'email-input';
+const PASSWORD_ID = 'password-input';
+const BTN_ID = 'login-submit-btn';
+const VALID_EMAIL = 'test@test.com';
+const VALID_PASSWORD = 'GROUP13';
 
 describe('Login tests', () => {
   beforeEach(async () => {
@@ -11,8 +18,19 @@ describe('Login tests', () => {
   afterEach(cleanup);
 
   it('Check screen elements', () => {
-    expect(screen.getByTestId('email-input')).toBeInTheDocument();
-    expect(screen.getByTestId('password-input')).toBeInTheDocument();
-    expect(screen.getByTestId('login-submit-btn')).toBeInTheDocument();
+    expect(screen.getByTestId(EMAIL_ID)).toBeInTheDocument();
+    expect(screen.getByTestId(PASSWORD_ID)).toBeInTheDocument();
+    expect(screen.getByTestId(BTN_ID)).toBeInTheDocument();
+  });
+
+  it('Check valid email and password', () => {
+    const emailLogin = screen.getByTestId(EMAIL_ID);
+    const password = screen.getByTestId(PASSWORD_ID);
+
+    userEvent.type(emailLogin, VALID_EMAIL);
+    userEvent.type(password, VALID_PASSWORD);
+
+    expect(emailLogin.value).toBe(VALID_EMAIL);
+    expect(password.value).toBe(VALID_PASSWORD);
   });
 });
